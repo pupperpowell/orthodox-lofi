63992e4238c6bc3b778a4e0ed43236cd9cf9bcb1
George Powell <geohpowell@gmail.com>
2025-02-26 17:38:31 -0500

    get port

--- main.ts
+++ main.ts
@@ -3,11 +3,13 @@
 /// <reference lib="dom.iterable" />
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
-
 import "$std/dotenv/load.ts";
-
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
 
-await start(manifest, config);
+// Get the port from environment variable or use 8000 as default
+const port = parseInt(Deno.env.get("PORT") || "8000");
+
+// Add the port to the config
+await start(manifest, { ...config, port });
